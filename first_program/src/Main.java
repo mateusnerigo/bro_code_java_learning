646cb24526b6fdd8f2c8509f5af169c7b65785c6
Mateus Neri <mateuslimaneri@gmail.com>
2024-01-05 09:29:16 -0300

    Gui dialogs

--- first_program/src/Main.java
+++ first_program/src/Main.java
@@ -1,10 +1,17 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // prints();
         // variables();
-        scanners();
+        // scanners();
+        guiDialogs();
+    }
+
+    public static void guiDialogs() {
+        String name = JOptionPane.showInputDialog("Enter you name");
+        JOptionPane.showMessageDialog(null, ("Hello " + name));
     }
 
     public static void scanners() {
